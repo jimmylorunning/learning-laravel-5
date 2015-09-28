1d53c6348ed6a5d6df89fef70446cd78b4c4c9a1
jimmylorunning <jimmylorunning@gmail.com>
2015-09-28 11:11:46 -0400

    Laracast video 19 (Manage your assets) complete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
-
+Route::get('/', function() {
+  return 'Home Page';
+});
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 /*
--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,22 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
+
+    mix.phpUnit();
+
     mix.sass('app.scss');
+
+    mix.styles([
+      'vendor/normalize.css',
+      'app.css'
+    ], null, 'public/css');
+
+// second argument (null) means output will be all.css, but you can also specify something like final/output.css
+    mix.version('public/css/all.css');
+
+/*
+  mix.coffee("*", "public/css", {
+            sass: "resources/assets/scss"
+        })
+*/
 });
